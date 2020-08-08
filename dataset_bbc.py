3cc51ba5afa4e938d1ca8e0f9d145859d723ec8a
Jordana <jordana.cbac@gmail.com>
2020-08-08 12:20:31 -0300

    Initial commit

---
+++ dataset_bbc.py
@@ -0,0 +1,24 @@
+import csv
+import os
+import pandas
+
+# Cria um arquivo csv com o corpora da BBC.
+# Cada documento do corpora esta contido em uma linha do arquivo.
+# A sequência das linhas preserva a ordem original dos documentos do corpora.
+def BBC():
+    corpus = os.listdir("../corpus/bbc")
+    csvFile = open("bbc.csv", "w", encoding = "utf-8", newline = '')
+    writer = csv.writer(csvFile)
+    writer.writerow(["<text>"])
+    for folder in corpus:
+        subfolder = os.listdir(folder)
+        for file in subfolder:
+            sentence = open(folder + "/" + file, "r")
+            content = sentence.read()
+            writer.writerow([content])
+    csvFile.close()
+    dataSet = pandas.read_csv("bbc.csv")
+    dataSet.to_csv("bbc_input.csv", index = False)
+    pass
+
+BBC()
